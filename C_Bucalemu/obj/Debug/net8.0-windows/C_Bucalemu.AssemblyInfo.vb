f4852937e3fd7846b04c9610107a6c0ef70da41b
JuanValdesS <juan.valdess@usm.cl>
2025-03-18 16:35:52 -0300

    Actualizacion de carpeta

--- C_Bucalemu/obj/Debug/net8.0-windows/C_Bucalemu.AssemblyInfo.vb
+++ C_Bucalemu/obj/Debug/net8.0-windows/C_Bucalemu.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("C_Bucalemu"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+279a143249e7eb7ae47f2cca2b4b407e6dab6cf0"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b1f872a46b39ce3fef72236722da76fe75e9020"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("C_Bucalemu"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("C_Bucalemu"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
