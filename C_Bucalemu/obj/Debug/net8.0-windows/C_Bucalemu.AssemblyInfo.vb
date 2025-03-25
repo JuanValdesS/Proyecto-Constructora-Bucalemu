304677b3c70396a130547970fc24296b4e9798d1
JuanValdesS <juan.valdess@usm.cl>
2025-03-25 14:29:27 -0300

    Se Agrega gitignore.txt para archivos que causan problemas

--- C_Bucalemu/obj/Debug/net8.0-windows/C_Bucalemu.AssemblyInfo.vb
+++ C_Bucalemu/obj/Debug/net8.0-windows/C_Bucalemu.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("C_Bucalemu"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc7ba86e34ba55046567cc5bf9b25da7c1db1566"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8459889dc9694b5ab09ced8cd05626c72489806c"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("C_Bucalemu"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("C_Bucalemu"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
