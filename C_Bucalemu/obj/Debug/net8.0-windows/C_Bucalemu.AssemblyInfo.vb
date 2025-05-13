63623325f4e21ad8335d9e21febafafb5274bd78
diego <diego.vasquezr@usm.cl>
2025-05-13 18:39:05 -0400

    hola

--- C_Bucalemu/obj/Debug/net8.0-windows/C_Bucalemu.AssemblyInfo.vb
+++ C_Bucalemu/obj/Debug/net8.0-windows/C_Bucalemu.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("C_Bucalemu"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+459d17446511bd24cc3332ad95fd2a34fdea375a"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17edb9994101b60b2129c207bace295171a47d30"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("C_Bucalemu"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("C_Bucalemu"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
